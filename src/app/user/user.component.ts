62928e1f68a8c10cb13ca25f88a6bdde882c1114
Aslan <ramazan.arslan@optware.de>
2023-08-04 15:04:53 +0200

    Create a new two-way-binding component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  newUser = false;
+  userStatus = "No user added";
+  userName = "Shanks";
+
+  ngOnInit(): void {
+  }
+
+  constructor() {
+    setTimeout(() => {
+      this.newUser = true;
+    }, 2000);
+  }
+
+  createUser() {
+    this.userStatus = 'New User Added, User name is ' + this.userName;
+  }
+
+  updateUserName(event: Event) {
+    this.userName = (<HTMLInputElement>event.target).value;
+  }
+}
